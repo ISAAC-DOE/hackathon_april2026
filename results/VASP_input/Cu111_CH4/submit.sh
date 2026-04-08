faceb9535da0b87b83a9b0e1593cad992f7afc08
HailiJia <you@example.com>
2026-04-08 01:07:54 -0500

    Add interface adsorbate VASP workflow script and generated input files

---
+++ results/VASP_input/Cu111_CH4/submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -A mxxxx
+#SBATCH -q regular
+#SBATCH -J Cu111_CH4
+#SBATCH -t 8:00:00
+#SBATCH -N 2
+#SBATCH -C cpu
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=user@example.com
+
+module load vasp/6.4.3-cpu
+
+export OMP_NUM_THREADS=2
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+srun -n 128 -c 4 --cpu-bind=cores vasp_std
